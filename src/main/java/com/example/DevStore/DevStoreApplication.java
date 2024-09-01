eaf76565baede6e1b9e15fe1ec79807e9b7f884e
thiagoluzvilv <thiagoluz959@gmail.com>
2024-09-01 16:18:29 -0300

    Adicionado o EurekaServer, mas o EurekaClient (DevStore_Cliente) não quer funcionar

--- src/main/java/com/example/DevStore/DevStoreApplication.java
+++ src/main/java/com/example/DevStore/DevStoreApplication.java
@@ -2,12 +2,12 @@ package com.example.DevStore;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
+@EnableEurekaServer
 public class DevStoreApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(DevStoreApplication.class, args);
 	}
-
 }
